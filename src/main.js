6d7bf24291c314714aca9fa1d98496fd802ccb1c
Bianca-Ribeiro-0 <biancamelamed7@gmail.com>
2023-06-14 20:31:27 -0300

    seção faq

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 //Adicionando evento p/ escutar quando o DOM terminou de carregar
 document.addEventListener('DOMContentLoaded', function(){
     const buttons = document.querySelectorAll('[data-tab-button]') ; //pegando tds os botões da lista
-
+    const questions = document.querySelectorAll('[data-faq-question]');
     //Adicionando um evento ao clique de tds os botões com for
+
     for (let i = 0; i < buttons.length; i++){
         buttons[i].addEventListener('click', function(botao){
             const abaAlvo = botao.target.dataset.tabButton;
@@ -13,7 +14,18 @@ document.addEventListener('DOMContentLoaded', function(){
             botao.target.classList.add('shows__tabs__button--is-active');
         })
     }
+
+    for (let i = 0; i < questions.length; i++) { //acessando os 4 elementos do faq
+        questions[i].addEventListener('click', abreOuFechaResposta); //adicionando um evento e a function
+    }
 })
+
+function abreOuFechaResposta(elemento){   
+  const classe =  'faq__questions__item--is-open'; //criando constante classe que deve ser acessada com o id --is-open (pergunta)
+  const elementoPai = elemento.target.parentNode; //apos investigar no target temos o elemento filho (li), acessamos o elementoPai (li) este que deve ser removido
+  
+  elementoPai.classList.toggle(classe); //adicionando elemento toggle
+}
 
 function removeBotaoAtivo(){
     const buttons = document.querySelectorAll('[data-tab-button]');

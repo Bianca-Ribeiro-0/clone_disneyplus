b0f766c2431c443a81e22b5a9360521092bdccc7
Bianca-Ribeiro-0 <biancamelamed7@gmail.com>
2023-06-20 10:59:56 -0300

    configurando gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
 const gulp = require('gulp');    //Iniciando pacote gulp, sass, e compressor de imgs
 const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
+
+function scripts(){
+    return gulp.src('./src/scripts/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/css'));
+}
 
 function styles(){
     return gulp.src('./src/styles/*scss')   //Função da estilização do projeto, comprimindo e destino para pasta dist
@@ -14,7 +21,8 @@ function images(){
         .pipe(gulp.dest('./dist/images'));
 }
 
-exports.default = gulp.parallel(styles, images);        //Padrão gulp, definindo a função watch para observar os eventos em paralelo
+exports.default = gulp.parallel(styles, images, scripts);        //Padrão gulp, definindo a função watch para observar os eventos em paralelo
 exports.watch = function(){
     gulp.watch('./src/styles/*scss', gulp.parallel(styles))
+    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts))
 }
